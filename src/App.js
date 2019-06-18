4915a9a5da81a11442473a9e7e546ae88f08feea
Ilya Popov <kludge.exe@gmail.com>
2019-06-18 08:46:56 +0300

    data.json added; App class started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-    </div>
-  );
+        let genreList = [];
+        let authorList = [];
+
+        for (let k in this.props.books) {
+            if (genreList.indexOf(this.props.books[k].genre) === -1) {
+                genreList.push(this.props.books[k].genre);
+            }
+        }
+
+        for (let k in this.props.books) {
+            if (authorList.indexOf(this.props.books[k].author) === -1) {
+                authorList.push(this.props.books[k].author);
+            }
+        }
+
+        console.log(genreList);
+        console.log(authorList);
+    }
+
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     if (prevState.inputText !== this.state.inputText) {
+    //         this.sendAjax(this.state.inputText);
+    //         this.setState({requestSuccess: true})
+    //     }
+    // }
+
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Booklist App!</h1>
+                <h3>
+
+                </h3>
+            </div>
+        )
+    }
 }
 
-export default App;
+// export default App;
